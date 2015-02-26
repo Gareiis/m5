9fbeebee4850a7b4f4ffa56be12b832a83dda3ea
Thomas <tlontz1@gmail.com>
2015-02-26 13:44:16 -0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('header')
+	This is home
+@endsection
+
+
+@section('main_content')
+	<a href="customer/all">Customers</a>
+	<a href="item/all">Items</a>
+	<a href="invoice/all">Invoices</a>
+@endsection
